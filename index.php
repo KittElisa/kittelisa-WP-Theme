a6eeb39496fc88ca2fbebf360a7c7320e58a74c5
KittElisa <elisa@elisa-Satellite-A200.(none)>
2012-08-09 21:04:43 +0200

    lesson 4

--- index.php
+++ index.php
@@ -1,44 +1,14 @@
-<html>
-<head>
-</head>
+<?php get_header(); ?>
+<div id="container">
  
-<body>
-<div id="wrapper" class="hfeed">
-    <div id="header">
-        <div id="masthead">
-          
-            <div id="branding">
-            </div><!-- #branding -->
-             
-            <div id="access">
-            </div><!-- #access -->
-             
-        </div><!-- #masthead -->   
-    </div><!-- #header -->
+    <div id="content">
+    </div><!-- #content -->
      
-    <div id="main">
-        <div id="container">
-         
-            <div id="content">
-            </div><!-- #content -->
-             
-        </div><!-- #container -->
-         
-        <div id="primary" class="widget-area">
-        </div><!-- #primary .widget-area -->
-         
-        <div id="secondary" class="widget-area">
-        </div><!-- #secondary -->
-    </div><!-- #main -->
-     
-    <div id="footer">
-        <div id="colophon">
-         
-            <div id="site-info">
-            </div><!-- #site-info -->
-             
-        </div><!-- #colophon -->
-    </div><!-- #footer -->
-</div><!-- #wrapper -->
-</body>
-</html>
+</div><!-- #container -->
+ 
+<div id="primary" class="widget-area">
+</div><!-- #primary .widget-area -->
+ 
+<div id="secondary" class="widget-area">
+</div><!-- #secondary -->
+<?php get_footer(); ?>
