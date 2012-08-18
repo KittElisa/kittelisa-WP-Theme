7f861f158dd57d78f18f83643802bad97808de9e
KittElisa <elisa.mvg@gmail.com>
2012-08-18 20:39:26 +0200

    Jquery

--- functions.php
+++ functions.php
@@ -155,4 +155,14 @@ function is_sidebar_active( $index ){
     return false;
 } // end is_sidebar_active
 
+
+// Insertamos la libreria de jquery hosteada en google
+if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+function my_jquery_enqueue() {
+  wp_deregister_script('jquery');
+  wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js", false, null);
+  wp_enqueue_script('jquery');
+}
+
+
 ?>
